8dea509909f0df3e6b29ddb0cb0cbabd4ff15a9a
Chris Gummer <chris@chrisgummer.com>
2014-10-11 15:23:35 -0600

    Initial commit

---
+++ SpacerViews/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  SpacerViews
+//
+//  Created by Chris Gummer on 10/11/14.
+//  Copyright (c) 2014 Effortless Code. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override func preferredStatusBarStyle() -> UIStatusBarStyle {
+        return UIStatusBarStyle.LightContent
+    }
+
+}
+
